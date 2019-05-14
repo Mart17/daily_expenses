f6883605b7faa360006605b8309fa6f360e43b31
Martin Suja <martin.suja17@gmail.com>
2019-05-14 17:31:39 +0200

    Edit initial tests

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -1,49 +1,45 @@
-# Feature: Sign in
-#   As a user
-#   I want to sign in
-#   So I can visit protected areas of the site
-feature 'Sign in', :devise do
-
-  # Scenario: User cannot sign in if not registered
-  #   Given I do not exist as a user
-  #   When I sign in with valid credentials
-  #   Then I see an invalid credentials message
+describe 'the signin process', type: :feature do
   scenario 'user cannot sign in if not registered' do
-    signin('test@example.com', 'please123')
-    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+    visit '/users/sign_in'
+    within("#new_user") do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
-  # Scenario: User can sign in with valid credentials
-  #   Given I exist as a user
-  #   And I am not signed in
-  #   When I sign in with valid credentials
-  #   Then I see a success message
-  scenario 'user can sign in with valid credentials' do
-    user = FactoryBot.create(:user)
-    signin(user.email, user.password)
-    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
+  before :each do
+    @user = FactoryBot.create(:user)
   end
 
-  # Scenario: User cannot sign in with wrong email
-  #   Given I exist as a user
-  #   And I am not signed in
-  #   When I sign in with a wrong email
-  #   Then I see an invalid email message
   scenario 'user cannot sign in with wrong email' do
-    user = FactoryBot.create(:user)
-    signin('invalid@email.com', user.password)
-    expect(page).to have_content I18n.t 'devise.failure.not_found_in_database', authentication_keys: 'email'
+    visit '/users/sign_in'
+    within("#new_user") do
+      fill_in 'Email', with: 'user@wrong_email.com'
+      fill_in 'Password', with: @user.password
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
-  # Scenario: User cannot sign in with wrong password
-  #   Given I exist as a user
-  #   And I am not signed in
-  #   When I sign in with a wrong password
-  #   Then I see an invalid password message
   scenario 'user cannot sign in with wrong password' do
-    user = FactoryBot.create(:user)
-    signin(user.email, 'invalidpass')
-    expect(page).to have_content I18n.t 'devise.failure.invalid', authentication_keys: 'email'
+    visit '/users/sign_in'
+    within("#new_user") do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: 'invlad_password'
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
+  scenario 'user can sign in with valid credentials' do
+    visit '/users/sign_in'
+    within("#new_user") do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully'
+  end
 end
