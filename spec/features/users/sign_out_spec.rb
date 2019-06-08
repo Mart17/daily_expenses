2ca0bd4ef1a788bb57a830b2fba1382f6f7288d7
Martin Suja <mart17@users.noreply.github.com>
2019-06-08 14:02:55 +0200

    Test devise sign in/sign out

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -1,19 +1,16 @@
-# Feature: Sign out
-#   As a user
-#   I want to sign out
-#   So I can protect my account from unauthorized access
 feature 'Sign out', :devise do
+  let(:user) { create (:user) }
 
-  # Scenario: User signs out successfully
-  #   Given I am signed in
-  #   When I sign out
-  #   Then I see a signed out message
-  pending 'user signs out successfully' do
-    user = FactoryBot.create(:user)
-    signin(user.email, user.password)
+  scenario 'user signs out successfully' do
+    visit '/users/sign_in'
+    within("#new_user") do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+    end
+    click_button 'Log in'
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
-    click_link 'Sign out'
+
+    click_link 'Log out'
     expect(page).to have_content I18n.t 'devise.sessions.signed_out'
   end
-
 end
