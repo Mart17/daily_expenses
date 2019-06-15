cfb26ad43597f790f96ee7a7e89d3cf79ee5736e
Martin Suja <mart17@users.noreply.github.com>
2019-06-15 19:39:50 +0200

    Add authorization to api controllers

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,17 +1,15 @@
 class Api::V1::EntriesController < ApiController
-  #before_action :correct_user
-  # TODO use current_user.entry.build etc for user_id
+  before_action :set_entry, except: :create
+  before_action :correct_user, except: :create
 
 	def create
-		entry = Entry.create!(entry_params)
+		entry = current_user.entries.create!(entry_params)
 
 		render json: entry
 	end
 
 	def update
-		entry = Entry.find(params[:id])
-
-		entry.update_attributes(entry_params)
+		@entry.update_attributes(entry_params)
 
 		render json: entry
 	end
@@ -19,16 +17,20 @@ class Api::V1::EntriesController < ApiController
 	def destroy
 		entry = Entry.find(params[:id])
 
-		entry.destroy
+		@entry.destroy
 	end
 
 	private
 
 	def entry_params
-		params.require(:entry).permit(:amount, :currency)
+		params.require(:entry).permit(:amount, :currency, :user_id)
 	end
 
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
   def correct_user
-    # TODO current_user == ...
+    head :not_acceptable unless current_user == @user
   end
 end
