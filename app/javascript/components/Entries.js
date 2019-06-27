b2fbe040b3b17f5f9d444f470bcf6698e2e57e93
Martin Suja <mart17@users.noreply.github.com>
2019-06-27 17:35:22 +0200

    Add entries component

--- app/javascript/components/Entries.js
+++ app/javascript/components/Entries.js
@@ -1,10 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import Index from "./Index"
+
 class Entries extends React.Component {
+  constructor() {
+    super();
+    this.state = { entries: [] }
+  }
+
+  componentDidMount() {
+	  fetch('/api/v1/current_user_entries.json')
+	  	.then((response) => {
+        return response.json()
+      })
+	  	.then((data) => {
+        this.setState({ entries: data })
+      })
+  }
+
   render () {
     return (
       <React.Fragment>
-        Your Entries
+        <Index entries={this.state.entries} />
       </React.Fragment>
     );
   }
