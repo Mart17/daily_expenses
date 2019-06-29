b2a637fa96ff3258c05298d9e1ce8612d5e33da3
Martin Suja <martin.suja17@gmail.com>
2019-06-29 21:33:31 +0200

    Organize current user entries by date

--- app/controllers/api/v1/current_user_entries_controller.rb
+++ app/controllers/api/v1/current_user_entries_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::CurrentUserEntriesController < ApiController
   def index
-		render json: current_user.entries
+    entries = current_user.entries.group_by { |t| t.created_at.to_date }
+
+    organized_by_date = []
+    entries.each { |e| organized_by_date << { date: e.first, entries: e.second } }
+
+    render json: organized_by_date
 	end
 end
