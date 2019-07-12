28d089a8600752b02ae84a79b9010efd3fbe8825
Martin Suja <mart17@users.noreply.github.com>
2019-07-12 21:31:08 +0200

    Add creating entries with react

--- app/javascript/components/Entries.jsx
+++ app/javascript/components/Entries.jsx
@@ -1,12 +1,41 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import Form from "./Form"
 import Index from "./Index"
 
 class Entries extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = { grouped_entries: [] }
+  }
+
+  handleCreate = new_entry => {
+    const body = JSON.stringify({ entry: { name: new_entry.name,
+                                           amount: new_entry.amount,
+                                           currency: new_entry.currency } })
+
+    fetch('api/v1/entries.json', {
+      method: 'POST',
+      headers: {
+				'Content-Type': 'application/json'
+			},
+      body: body
+    }).then((response) => {
+      return response.json()
+    })
+    .then((new_entry_data) => {
+      this.createEntry(new_entry_data)
+    })
+  }
+
+  createEntry = new_entry => {
+    this.setState({
+      // FIXME but this can create new group so it can't be with 0
+      // first need to ask if the date is the same as the date of the last group?
+      // if yes then append, if no then need new date group
+			grouped_entries: this.state.grouped_entries[0].concat(new_entry)
+		})
   }
 
   componentDidMount() {
@@ -22,9 +51,11 @@ class Entries extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <Form handleCreate={this.handleCreate} />
+        <br />
         <Index groupedEntries={this.state.grouped_entries} />
       </React.Fragment>
-    );
+    )
   }
 }
 
--- spec/javascript/index.spec.js
+++ spec/javascript/index.spec.js
@@ -4,11 +4,11 @@ import Index from 'components/Index'
 
 describe('Index component', () => {
   it('shows the data properly', () => {
-    const data = [{"date":"2019-06-27",
-                   "entries":[{"id":7,"amount":"5.55","currency":"USD","user_id":3,
-                               "created_at":"2019-06-27T18:17:02.504Z",
-                               "updated_at":"2019-06-27T18:17:02.504Z",
-                               "name":"netflix"}]
+    const data = [{ "date":"2019-06-27",
+                    "entries":[{ "id":7,"amount":"5.55","currency":"USD","user_id":3,
+                                 "created_at":"2019-06-27T18:17:02.504Z",
+                                 "updated_at":"2019-06-27T18:17:02.504Z",
+                                 "name":"netflix" }]
                  }]
 
     const wrapper = shallow(<Index groupedEntries={data} />)
