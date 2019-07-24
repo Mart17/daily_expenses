8236d8735b8e13da3875764445e545fde8322539
Martin Suja <mart17@users.noreply.github.com>
2019-07-24 20:45:07 +0200

    Change Index.jsx to functional component

--- app/javascript/components/Index.jsx
+++ app/javascript/components/Index.jsx
@@ -5,68 +5,62 @@ import { localDate } from '../utils/Localization.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-class Index extends React.Component {
-  handleChange = (e, id) => {
-    this.props.handleUpdate(id, e.target.name, e.target.value)
-  }
-
-  render () {
-    const entry_segments = this.props.groupedEntries.map((group, group_index) => {
-      let entries = group.entries.map((entry) => {
-        return (
-          <div key={entry.id}>
-            <form className="form-inline">
-              <input
-                className="form-control input-spaced"
-                placeholder="Name"
-                type="text"
-                name="name"
-                defaultValue={entry.name}
-                onChange={(e) => this.handleChange(e, entry.id)} />
-              <input
-                className="form-control input-spaced"
-                placeholder="Amount"
-                type="text"
-                size="8"
-                name="amount"
-                defaultValue={entry.amount}
-                onChange={(e) => this.handleChange(e, entry.id)} />
-              <input
-                className="form-control input-spaced"
-                placeholder="Currency"
-                type="text"
-                size="3"
-                name="currency"
-                defaultValue={entry.currency}
-                onChange={(e) => this.handleChange(e, entry.id)} />
-              <FontAwesomeIcon
-                icon={faTrashAlt}
-                onClick={() => { if (window.confirm('Are you sure?'))
-                                    this.props.handleDelete(entry.id, group_index) }}/>
-            </form>
-          </div>
-        )
-      })
-
+const Index = (props) => {
+  const entry_segments = props.groupedEntries.map((group, group_index) => {
+    let entries = group.entries.map((entry) => {
       return (
-        <div key={group.date}>
-          <b>
-            {localDate(group.date, 'en-US')}
-          </b>
-          <br />
-          {entries}
-          <br />
+        <div key={entry.id}>
+          <form className="form-inline">
+            <input
+              className="form-control input-spaced"
+              placeholder="Name"
+              type="text"
+              name="name"
+              defaultValue={entry.name}
+              onChange={(e) => props.handleUpdate(entry.id, e.target.name, e.target.value)} />
+            <input
+              className="form-control input-spaced"
+              placeholder="Amount"
+              type="text"
+              size="8"
+              name="amount"
+              defaultValue={entry.amount}
+              onChange={(e) => props.handleUpdate(entry.id, e.target.name, e.target.value)} />
+            <input
+              className="form-control input-spaced"
+              placeholder="Currency"
+              type="text"
+              size="3"
+              name="currency"
+              defaultValue={entry.currency}
+              onChange={(e) => props.handleUpdate(entry.id, e.target.name, e.target.value)} />
+            <FontAwesomeIcon
+              icon={faTrashAlt}
+              onClick={() => { if (window.confirm('Are you sure?'))
+                                props.handleDelete(entry.id, group_index) }}/>
+          </form>
         </div>
       )
     })
 
     return (
-      <div>
-        <h4>Your Entries</h4>
-        {entry_segments}
+      <div key={group.date}>
+        <b>
+          {localDate(group.date, 'en-US')}
+        </b>
+        <br />
+        {entries}
+        <br />
       </div>
     )
-  }
+  })
+
+  return (
+    <div>
+      <h4>Your Entries</h4>
+      {entry_segments}
+    </div>
+  )
 }
 
 export default Index
