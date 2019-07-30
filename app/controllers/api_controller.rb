f894b47bba18630a2179475349406317e902bc4d
Martin Suja <mart17@users.noreply.github.com>
2019-07-30 18:06:32 +0200

    Add authenticity token to users

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,16 @@
 class ApiController < ApplicationController
   before_action :set_default_response_format
+  before_action :authenticate_token!
   before_action :authenticate_user!
 
   private
+
+  def authenticate_token!
+    # TODO or missing ['Authentication-Token']
+    if request.headers['Authentication-Token'] != current_user.authenticity_token
+      render status: :unauthorized, json: { error: "You're missing correct authenticity token." }
+    end
+  end
 
   def set_default_response_format
     request.format = :json
