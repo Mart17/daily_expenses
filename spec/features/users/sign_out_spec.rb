297b22dccefcd63fa29aebf1381ec8c15810f3b4
Martin Suja <mart17@users.noreply.github.com>
2019-07-31 20:57:46 +0200

    Fix signing in specs and remove devise_error_messages helper from views

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -9,7 +9,7 @@ feature 'Sign out', :devise do
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
     end
-    click_button 'Log in'
+    click_button 'Sign in'
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
 
     expect(page).to have_content 'Log out'
