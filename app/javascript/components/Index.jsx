749f89f1ccafadd3d3703d7e3e2c2a2fb3792d37
Martin Suja <mart17@users.noreply.github.com>
2019-08-02 21:46:09 +0200

    Add static calculations for group

--- app/javascript/components/Index.jsx
+++ app/javascript/components/Index.jsx
@@ -19,6 +19,31 @@ const Index = (props) => {
     }
   }
 
+  const groupCalculations = (group) => {
+    // find all used currencies
+    const usedCurrencies  = [...new Set(group.entries.map(e => e.currency))]
+
+    // calculate sum for each currency
+    const currencyAmounts = {}
+    usedCurrencies.forEach(function(currency) {
+      let currencyEntries = group.entries.filter(entry => {
+        return entry.currency === currency
+      })
+
+      currencyAmounts[currency] = currencyEntries.reduce(
+        // multiple by 100 to treat cents as integers for now
+        // this is to avoid js problems with addition of decimals
+        (total, entry) => total + (parseFloat(entry.amount) * 100), 0)
+    })
+
+    const displaySums = Object.keys(currencyAmounts).map((key, index) =>
+      // here divide by 100 to make cents decimals again
+      `${key}: ${Object.values(currencyAmounts)[index] / 100}`
+    )
+
+    return displaySums.join(', ')
+  }
+
   const entrySegments = props.groupedEntries.map((group, groupIndex) => {
     let entries = group.entries.map((entry) => {
       return (
@@ -66,6 +91,7 @@ const Index = (props) => {
         <b>
           {localDate(group.date, 'en-US')}
         </b>
+        <span> ({groupCalculations(group)})</span>
         <br />
         <div className="entry-group">
           {entries}
