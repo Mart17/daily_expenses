6dd72dcaf26ee2542e4ca19fe2ba0e5f9484f0c2
Martin Suja <mart17@users.noreply.github.com>
2019-08-21 17:51:16 +0200

    Remove associated entries on canceling user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   has_secure_token :authenticity_token
 
-  has_many :entries
+  has_many :entries, dependent: :destroy
 
   validates_presence_of :name, :default_currency
 
