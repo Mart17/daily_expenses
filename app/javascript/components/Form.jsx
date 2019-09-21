05f4556d2a53b437a5973cee3c0cdd2b0f7df14f
Martin Suja <mart17@users.noreply.github.com>
2019-09-21 15:39:17 +0200

    Add background image & change fieldset background color

--- app/javascript/components/Form.jsx
+++ app/javascript/components/Form.jsx
@@ -31,37 +31,37 @@ class Form extends React.Component {
         <fieldset>
           <h4>New Entry</h4>
           <form className="form-inline">
-              <input
-                className="form-control form-control-lg input-spaced"
-                placeholder="Name"
-                type="text"
-                maxLength="50"
-                name="name"
-                value={this.state.entry.name}
-                onChange={this.handleChange} />
-              <input
-                className="form-control form-control-lg input-spaced"
-                placeholder="Amount"
-                type="number"
-                step="any"
-                name="amount"
-                value={this.state.entry.amount}
-                onChange={this.handleChange} />
-              <select
-                className="form-control form-control-lg input-spaced"
-                name="currency"
-                title="Change default currency in the settings"
-                value={this.state.entry.currency}
-                onChange={this.handleChange} >
-                <option value="€">€</option>
-                <option value="$">$</option>
-                <option value="£">£</option>
-              </select>
-              <input
-                type="button"
-                className="submit"
-                value="Submit"
-                onClick={this.submitForm} />
+            <input
+              className="form-control form-control-lg input-spaced"
+              placeholder="Name"
+              type="text"
+              maxLength="50"
+              name="name"
+              value={this.state.entry.name}
+              onChange={this.handleChange} />
+            <input
+              className="form-control form-control-lg input-spaced"
+              placeholder="Amount"
+              type="number"
+              step="any"
+              name="amount"
+              value={this.state.entry.amount}
+              onChange={this.handleChange} />
+            <select
+              className="form-control form-control-lg input-spaced"
+              name="currency"
+              title="Change default currency in the settings"
+              value={this.state.entry.currency}
+              onChange={this.handleChange} >
+              <option value="€">€</option>
+              <option value="$">$</option>
+              <option value="£">£</option>
+            </select>
+            <input
+              type="button"
+              className="submit"
+              value="Submit"
+              onClick={this.submitForm} />
           </form>
         </fieldset>
       </div>
